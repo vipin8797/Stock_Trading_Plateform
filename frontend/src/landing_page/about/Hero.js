fdac1d7ffd58493c1d90ed416ad2ea6ee44ab1b7
vipin yadav <cy7795151@gmail.com>
2025-09-27 19:21:59 +0530

    routes setup in react

---
+++ frontend/src/landing_page/about/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Hero() {
+    return (  
+        <div className="container">
+           <div className="row">
+            <h1 className="text-center">
+                We pioneered the discount broking model in India.
+            </h1>
+           </div>
+        </div>
+    );
+}
+
+export default Hero;
+<div className="container">
+           <div className="row">
+            <h1>
+                Now, we are breaking ground with our technology.
+            </h1>
+           </div>
+</div>
