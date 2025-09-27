ca786fb237bb48f52c031804ede1faca4886b1eb
vipin yadav <cy7795151@gmail.com>
2025-09-27 19:04:06 +0530

    react routes set

---
+++ frontend/src/landing_page/products/ProductPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Hero from "./Hero";
+import LeftSection from "./LeftSection";
+import RightSection from "./RightSection";
+
+function ProductPage() {
+    return ( 
+        <>
+        <Hero/>
+        <LeftSection/>
+        <RightSection/>
+        
+        </>
+     );
+}
+
+export default ProductPage;
