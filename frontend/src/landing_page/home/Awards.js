806c232ed18b9ce76eba015cd15f05f89065becb
vipin yadav <cy7795151@gmail.com>
2025-09-27 15:01:23 +0530

    Hero section and Award section creted and styles.

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -2,9 +2,34 @@ import React from 'react';
 
 function Awards() {
     return ( 
-        <>
-        <h1>Awards Component <i class="fa fa-home"></i></h1>
-        </>
+       <div  className="container mt-5">
+          <div className="row p-5">
+            <div className="col-6">
+                <img src="/media/images/largestBroker.svg"></img>
+            </div>
+            <div className="col-6 p-5 ">
+                <h1>Largest Sock Broker in India.</h1>
+                <p>2+ millions Zerodha clients contribute to over 15% of all retial order volume in indai daily by trading and investing in:</p>
+               <div className="row">
+                    <div className="col-6">
+                        <ul>
+                            <li>Future and Options</li>
+                            <li>Commodity and Derivatives</li>
+                            <li>Currency Derivatives</li>
+                        </ul>
+                    </div>
+                    <div className="col-6">
+                         <ul>
+                            <li>Stocks & IPOs</li>
+                            <li>Direct Mutual funds</li>
+                            <li>Bonds & Gov. Securities</li>
+                        </ul>
+                    </div>
+               </div>
+               <img src="/media/images/pressLogos.png" style={{width:"90%"}}></img>
+            </div>
+          </div>
+       </div>
      );
 }
 
--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 function Hero() {
     return ( 
-        <>
-        <h1>Hero Components <i class="fa-regular fa-face-grin-hearts"></i></h1>
-        </>
+       <div  className="container mb-5 pb-5">
+         <div className="row text-center">
+            <img src="/media/images/homeHero.png" alt="Hero Image" className="mb-5"></img>
+            <h1 className="mt-5">Inverst in Everything</h1>
+            <p>oneline plateform to invest in stocks,derivative mutual funds.</p>
+            <button type="button" style={{width:"20%", margin:"0 auto"}} class="btn btn-primary btn-lg">SignUp Now</button>
+         </div>
+       </div>
      );
 }
 
