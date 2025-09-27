e3d819d24a6c066999938e8b5608896ad523e7a6
vipin yadav <cy7795151@gmail.com>
2025-09-27 20:28:04 +0530

    about component styled.

--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -2,8 +2,26 @@ import React from 'react';
  
 function Team() {
     return (  
-        <>
-        <h1>Team Component</h1></>
+        <div className="container">
+           <div className="row p-5 text-center">
+           <h1 className="mb-5" style={{fontSize:"35px"}}>People</h1>
+           <div className="col text-center p-5">
+                <img src="/media/images/nithinKamath.jpg" style={{borderRadius:"100%",width:"60%"}}></img>
+                <h2 className="mt-2">Nithin Kamath</h2>
+                <p>Founder & CEO</p>
+           </div>
+
+           <div className="col p-5 fs-5 text-muted ">
+           <p> Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry.</p>
+
+<p>He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).</p>
+
+<p>Playing basketball is his zen.</p>
+
+<p>Connect on <a href="">HomePage</a>/<a href="">Trading Q&A</a>/<a href="">Twitter</a></p>
+           </div>
+           </div>
+        </div>
     );
 }
 
